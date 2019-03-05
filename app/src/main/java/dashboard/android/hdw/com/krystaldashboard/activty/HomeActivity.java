5e16850669754135b05e037bc1cc90b71de64e5e
PCHRP <fluk.fluk.zuza@gmail.com>
2019-03-05 16:53:40 +0700

    แก้ชื่อ จาก MAIN เป็น HOME

--- app/src/main/java/dashboard/android/hdw/com/krystaldashboard/activty/HomeActivity.java
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/activty/HomeActivity.java
@@ -1,62 +1,94 @@
 package dashboard.android.hdw.com.krystaldashboard.activty;
 
-import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Calendar;
-
 import dashboard.android.hdw.com.krystaldashboard.R;
+import dashboard.android.hdw.com.krystaldashboard.fragment.DrinkFragment;
+import dashboard.android.hdw.com.krystaldashboard.fragment.HomeFragment;
+import dashboard.android.hdw.com.krystaldashboard.fragment.PRFragment;
+import dashboard.android.hdw.com.krystaldashboard.fragment.RevenueFragment;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView textView;
+    Toolbar toolbar;
+    BottomNavigationView bottomNavigationView;
 
-    private long pausedMillis;
-    private Boolean c = false;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.item_logout) {
+            Toast.makeText(HomeActivity.this, "Logout", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+        textView = findViewById(R.id.text);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.item_home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.item_revrnue:
+                        fragment = new RevenueFragment();
+                        break;
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        pausedMillis = Calendar.getInstance().getTimeInMillis();
-        c = true;
-    }
+                    case R.id.item_drink:
+                        fragment = new DrinkFragment();
+                        break;
 
+                    case R.id.item_pr:
+                        fragment = new PRFragment();
+                        break;
+                }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        try {
-            long currentMillis = Calendar.getInstance().getTimeInMillis();
-            long aa = currentMillis - pausedMillis;
-            if ( c == true && currentMillis - pausedMillis  > 1000 * 60 * 1 ) {
-                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
-                finish();
-                System.out.println("trueeeeeeee");
-                // Toast.makeText(HomeActivity.this, "Log out", Toast.LENGTH_LONG).show();
+                return loadFragment(fragment);
             }
-        } catch (Exception e){
-            e.printStackTrace();
-            System.out.println("falseeeeeee");
+        });
+bottomNavigationView.setSelectedItemId(R.id.item_home);
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
         }
 
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
+        return false;
     }
 }
