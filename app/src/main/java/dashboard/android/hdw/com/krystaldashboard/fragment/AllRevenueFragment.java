259624fb1a8309f4909d53915a6416e630a02fb0
PCHRP <fluk.fluk.zuza@gmail.com>
2019-03-07 16:42:07 +0700

    แยกฟังก์ชั่นต่างๆ เป็น fragment

---
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/fragment/AllRevenueFragment.java
@@ -0,0 +1,26 @@
+package dashboard.android.hdw.com.krystaldashboard.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import dashboard.android.hdw.com.krystaldashboard.R;
+
+public class AllRevenueFragment extends Fragment {
+
+    public AllRevenueFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View AllRevenue = inflater.inflate(R.layout.fragment_allrevenue,container,false);
+
+        return AllRevenue;
+    }
+}
