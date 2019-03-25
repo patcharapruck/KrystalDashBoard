b66728aefb5b3f2139d3ceda0b8dcf2d88b5a3cc
PCHRP <fluk.fluk.zuza@gmail.com>
2019-03-25 11:25:34 +0700

    สร้าง singleton สถานะการชำระ

--- app/src/main/java/dashboard/android/hdw/com/krystaldashboard/manager/singleton/PayManager.java
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/manager/singleton/PayManager.java
@@ -1,4 +1,31 @@
 package dashboard.android.hdw.com.krystaldashboard.manager.singleton;
 
+import android.content.Context;
+
+import dashboard.android.hdw.com.krystaldashboard.dto.paymentstatus.PayItemColleationDto;
+import dashboard.android.hdw.com.krystaldashboard.manager.Contextor;
+
 public class PayManager {
+
+    private static PayManager instance;
+    private PayItemColleationDto payItemColleationDao;
+    public static PayManager getInstance() {
+        if (instance == null)
+            instance = new PayManager();
+        return instance;
+    }
+
+    public PayItemColleationDto getPayItemColleationDao() {
+        return payItemColleationDao;
+    }
+
+    public void setPayItemColleationDao(PayItemColleationDto payItemColleationDao) {
+        this.payItemColleationDao = payItemColleationDao;
+    }
+
+    private Context mContext;
+
+    private PayManager() {
+        mContext = Contextor.getInstance().getmContext();
+    }
 }
