c4993488d6417bcbe0f81d5478559358cd10fea8
PCHRP <fluk.fluk.zuza@gmail.com>
2019-03-29 11:17:39 +0700

    เพิ่มจำนวนแสดงช่องทางการชำระเงิน

--- app/src/main/java/dashboard/android/hdw/com/krystaldashboard/fragment/CraditFragment.java
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/fragment/CraditFragment.java
@@ -2,7 +2,6 @@ package dashboard.android.hdw.com.krystaldashboard.fragment;
 
 
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -35,30 +34,19 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
 
 
     BarChart barChart;
+    TextView TotalSum,TotalKungthap,TotalTanachat,TotalTthaipanich,TotalKhunoot;
+    Double creditall = 0.0, amax = 0.0, jcb = 0.0, master = 0.0, unipay = 0.0, visa = 0.0;
 
-    TextView CreditallTextView,
-            AmaxTTextView, AmaxKTextView,
-            JcbTTextView, JcbKTextView,
-            MasterTTextView, MasterKTextView,
-            UnipayTTextView, UnipayKTextView,
-            VisaTextView, VisaKTextView,
-            TotalTTextView, TotalKTextView;
+    ArrayList<Double> total = new ArrayList<>();
 
-    Double creditall,
-            amaxt = 0.0, amaxk = 0.0,
-            jcbt = 0.0, jcbk = 0.0,
-            mastert = 0.0, masterk = 0.0,
-            unipayt = 0.0, unipayk = 0.0,
-            visat = 0.0, visak = 0.0,
-            totalt = 0.0, totalk = 0.0;
+    ArrayList<String> creditAll = new ArrayList<>();
+    ArrayList<String> Amax = new ArrayList<>();
+    ArrayList<String> Jcb = new ArrayList<>();
+    ArrayList<String> Master = new ArrayList<>();
+    ArrayList<String> Unipay = new ArrayList<>();
+    ArrayList<String> Visa = new ArrayList<>();
+    ArrayList<String> Total = new ArrayList<>();
 
-    String creditalls,
-            amaxts, amaxks,
-            jcbts, jcbks,
-            masterts, masterks,
-            unipayts, unipayks,
-            visats, visaks,
-            totalts,totalks;
 
     LinearLayout creditA,creditB;
     Button creditABtn,creditBBtn;
@@ -68,7 +56,6 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
     public CraditFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -92,8 +79,13 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
 
         formatter = new DecimalFormat("#,###,##0.00");
 
-        TotalTTextView = (TextView) rootView.findViewById(R.id.textview_totalt);
-        TotalKTextView = (TextView) rootView.findViewById(R.id.textview_totalk);
+        TotalSum = (TextView) rootView.findViewById(R.id.textview_sum);
+        TotalKungthap = (TextView) rootView.findViewById(R.id.textview_kungthap);
+        TotalTthaipanich = (TextView) rootView.findViewById(R.id.textview_thaipanich);
+        TotalTanachat = (TextView) rootView.findViewById(R.id.textview_tanachat);
+        TotalKhunoot = (TextView) rootView.findViewById(R.id.textview_khunoot);
+
+
 
         try {
             setdata();
@@ -105,64 +97,65 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
 
     private void setdata() {
 
-        CreditItemColleationDto   Credit1 = DashBoradManager.getInstance().getDao().getObject().getIncomeByCreditCardList().get(0);
-        CreditItemColleationDto   Credit2 = DashBoradManager.getInstance().getDao().getObject().getIncomeByCreditCardList().get(1);
 
-        creditall = DashBoradManager.getInstance().getDao().getObject().getCreditCardPayments();
+        //creditall = DashBoradManager.getInstance().getDao().getObject().getCreditCardPayments();
 
-        amaxt = Credit1.getAmax();
-        amaxk = Credit2.getAmax();
-        jcbt = Credit1.getJcb();
-        jcbk = Credit2.getJcb();
-        mastert = Credit1.getMaster();
-        masterk = Credit2.getMaster();
-        unipayt = Credit1.getUnipay();
-        unipayk = Credit2.getUnipay();
-        visat = Credit1.getVisa();
-        visak = Credit2.getVisa();
+        for(int i=0;true;i++){
+            CreditItemColleationDto  Credit;
+            try{
+              Credit = DashBoradManager.getInstance().getDao()
+                        .getObject().getIncomeByCreditCardList().get(i);
+            }catch (Exception e){
+                break;
+            }
 
-        totalt = amaxt+jcbt+mastert+unipayt+visat;
-        totalk = amaxk+jcbk+masterk+unipayk+visak;
+            amax = Credit.getAmax();
+            jcb = Credit.getJcb();
+            master = Credit.getMaster();
+            unipay = Credit.getUnipay();
+            visa = Credit.getVisa();
 
-        totalts = formatter.format(totalt);
-        totalks = formatter.format(totalk);
-
-        TotalTTextView.setText(totalts);
-        TotalTTextView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-
-        TotalKTextView.setText(totalks);
-        TotalKTextView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
-
-        ArrayList<BarEntry> values = new ArrayList<>();
-        ArrayList<BarEntry> values2 = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-
-            float val1 = bar_B1().get(i);
-            float val2 = bar_B2().get(i);
-            values.add(new BarEntry(i, new float[]{val1}));
-            values2.add(new BarEntry(i, new float[]{val2}));
+            total.add(i,amax + jcb + master + unipay + visa);
+            creditall = creditall+total.get(i);
+            Total.add(i,formatter.format(total.get(i)));
         }
 
 
-        BarDataSet set1;
+        TotalSum.setText(formatter.format(creditall));
+        TotalKungthap.setText(Total.get(1));
+        TotalTanachat.setText(Total.get(0));
+        TotalTthaipanich.setText(Total.get(2));
+        TotalKhunoot.setText(Total.get(3));
 
+//        TotalSum.setText(totalts);
+//        TotalSum.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+//
+//        TotalKTextView.setText(totalks);
+//        TotalKTextView.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        ArrayList<BarEntry> values = new ArrayList<>();
+
+        for (int i = 0; true ; i++) {
+
+            try {
+                values.add(new BarEntry(i,total.get(i).floatValue()));
+            }catch (Exception e){
+                break;
+            }
+
+        }
+
+        BarDataSet set1;
         set1 = new BarDataSet(values,"Credit");
         set1.setColors(Color.parseColor("#001B7A"));
         set1.setValueTextColor(Color.BLACK);
 
-        BarDataSet set2;
 
-        set2 = new BarDataSet(values2,"Credit");
-        set2.setColors(Color.parseColor("#f37023"));
-        set2.setValueTextColor(Color.BLACK);
-
-
-        BarData data = new BarData(set1,set2);
+        BarData data = new BarData(set1);
         data.setValueFormatter(new MyFormatCredit());
         barChart.setData(data);
 
-        String[] creditName = new String[]{"A-MAX ", " JCB ", "MASTER", "UNIPAY", "VISA"};
+        String[] creditName = new String[]{"กรุงเทพ","ธนชาต","ไทยพาณิชย์","คุณอ๊อต"};
         XAxis xAxis = barChart.getXAxis();
         xAxis.setValueFormatter(new IndexAxisValueFormatter(creditName));
         xAxis.setCenterAxisLabels(true);
@@ -170,8 +163,8 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
         xAxis.setGranularity(1);
         xAxis.setGranularityEnabled(true);
 
-        barChart.setDragEnabled(true);
-        barChart.setVisibleXRangeMaximum(5);
+        barChart.setDragEnabled(false);
+        barChart.setVisibleXRangeMaximum(4);
 
         //set Label Center
         float barSpace = 0.05f;
@@ -196,29 +189,29 @@ public class CraditFragment extends Fragment implements View.OnClickListener {
         barChart.invalidate();
     }
 
-    private ArrayList<Float> bar_B1() {
-        ArrayList<Float> barBnk1 = new ArrayList<>();
-        barBnk1.add(0, (float) ((amaxt/creditall)*100));
-        barBnk1.add(1, (float) ((jcbt/creditall)*100));
-        barBnk1.add(2, (float) ((mastert/creditall)*100));
-        barBnk1.add(3, (float) ((unipayt/creditall)*100));
-        barBnk1.add(4, (float) ((visat/creditall)*100));
-
-        return barBnk1;
-    }
-
-    //BBL(ธนาคารกรุงเทพ)
-    private ArrayList<Float> bar_B2() {
-        ArrayList<Float> barBnk2 = new ArrayList<>();
-
-        barBnk2.add(0, (float) ((amaxk/creditall)*100));
-        barBnk2.add(1, (float) ((jcbk/creditall)*100));
-        barBnk2.add(2, (float) ((masterk/creditall)*100));
-        barBnk2.add(3, (float) ((unipayk/creditall)*100));
-        barBnk2.add(4, (float) ((visak/creditall)*100));
-
-        return barBnk2;
-    }
+//    private ArrayList<Float> bar_B1() {
+//        ArrayList<Float> barBnk1 = new ArrayList<>();
+//        barBnk1.add(0, (float) ((amax /creditall)*100));
+//        barBnk1.add(1, (float) ((jcb /creditall)*100));
+//        barBnk1.add(2, (float) ((master /creditall)*100));
+//        barBnk1.add(3, (float) ((unipay /creditall)*100));
+//        barBnk1.add(4, (float) ((visa /creditall)*100));
+//
+//        return barBnk1;
+//    }
+//
+//    //BBL(ธนาคารกรุงเทพ)
+//    private ArrayList<Float> bar_B2() {
+//        ArrayList<Float> barBnk2 = new ArrayList<>();
+//
+//        barBnk2.add(0, (float) ((amaxk/creditall)*100));
+//        barBnk2.add(1, (float) ((jcbk/creditall)*100));
+//        barBnk2.add(2, (float) ((masterk/creditall)*100));
+//        barBnk2.add(3, (float) ((unipayk/creditall)*100));
+//        barBnk2.add(4, (float) ((visak/creditall)*100));
+//
+//        return barBnk2;
+//    }
 
     @Override
     public void onClick(View v) {
