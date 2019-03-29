578feaf30d0b85b10cb5972b1633c91fbfc5c0c8
Naphatsanan <naphatsanan.boo@msu.ac.th>
2019-03-29 16:44:59 +0700

    ui

---
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/fragment/Revanuef2Fragment.java
@@ -0,0 +1,46 @@
+package dashboard.android.hdw.com.krystaldashboard.fragment;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import dashboard.android.hdw.com.krystaldashboard.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Revanuef2Fragment extends Fragment {
+
+
+    public Revanuef2Fragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_revanue_f2,container,false);
+        initInstances(rootView);
+        return rootView;
+
+
+
+
+    }
+
+    private void initInstances(View rootView) {
+
+
+
+
+    }
+
+}
