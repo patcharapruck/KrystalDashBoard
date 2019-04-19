012d101f66aacf3a6f7663d0ee0b9e6e9fd0a3d2
PCHRP <fluk.fluk.zuza@gmail.com>
2019-04-19 18:04:03 +0700

    list เครื่องดื่ม

--- app/src/main/java/dashboard/android/hdw/com/krystaldashboard/adapter/ProductListAdapter.java
+++ app/src/main/java/dashboard/android/hdw/com/krystaldashboard/adapter/ProductListAdapter.java
@@ -4,12 +4,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import dashboard.android.hdw.com.krystaldashboard.dto.product.ProductSortDto;
+import dashboard.android.hdw.com.krystaldashboard.manager.singleton.ProductManager;
 import dashboard.android.hdw.com.krystaldashboard.view.CustomViewProduct;
 
 public class ProductListAdapter extends BaseAdapter {
     @Override
     public int getCount() {
-        return 18;
+        if(ProductManager.getInstance().getProductSortDto() == null)
+            return 0;
+        return ProductManager.getInstance().getProductSortDto().getNameProductSort().length;
     }
 
     @Override
@@ -30,6 +34,42 @@ public class ProductListAdapter extends BaseAdapter {
         }else{
             item = new CustomViewProduct(parent.getContext());
         }
+
+        ProductSortDto dto = ProductManager.getInstance().getProductSortDto();
+
+        try {
+            item.setNameProduct(position+1,dto.getNameProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setNameProduct(0,"null");
+        }
+        try {
+            item.setWithdrawProduct(dto.getWithdrawProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setWithdrawProduct(0L);
+        }
+        try {
+            item.setPurchaseProduct(dto.getPurchaseProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setPurchaseProduct(0L);
+        }
+        try {
+            item.setEntertainProduct(dto.getEntertainProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setEntertainProduct(0L);
+        }
+        try {
+            item.setTotalProduct(dto.getTotalAllProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setTotalProduct(0L);
+        }
+
+        try {
+            item.setImageProduct(dto.getImageProductSort()[position]);
+        }catch (NullPointerException e){
+            item.setImageProduct("");
+        }
+
+
 
         return item;
     }
